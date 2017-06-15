1104b7103ec41604a011ba36abb79b33a9ab3223
Janice Yura Lee <janiceyuralee@gmail.com>
2017-06-15 10:53:48 -0700

    Change valuable name

--- heap_and_heapsort/lib/heap_sort.rb
+++ heap_and_heapsort/lib/heap_sort.rb
@@ -2,12 +2,12 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    2.upto(length).each do |size|
-      BinaryMinHeap.heapify_up(self, size - 1, size)
+    2.upto(length).each do |start|
+      BinaryMinHeap.heapify_up(self, start - 1, start)
     end
-    length.downto(2).each do |size|
-      self[size - 1], self[0] = self[0], self[size - 1]
-      BinaryMinHeap.heapify_down(self, 0, size - 1)
+    length.downto(2).each do |start|
+      self[start - 1], self[0] = self[0], self[start - 1]
+      BinaryMinHeap.heapify_down(self, 0, start - 1)
     end
     self.reverse!
   end
