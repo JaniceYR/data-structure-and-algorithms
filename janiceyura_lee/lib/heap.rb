2b90c60cf57b89eb8734b4c431c92aaacffbbacd
Janice Yura Lee <janiceyuralee@gmail.com>
2017-06-14 20:00:20 -0700

    Add heap

--- janiceyura_lee/lib/heap.rb
+++ janiceyura_lee/lib/heap.rb
@@ -1,31 +1,74 @@
+require 'byebug'
+
 class BinaryMinHeap
   attr_reader :store, :prc
 
   def initialize(&prc)
+    @prc = prc
+    @store = Array.new
   end
 
   def count
+    @store.length
   end
 
   def extract
+    first = @store.shift
+    @store = BinaryMinHeap.heapify_down(@store, 0, @prc)
+    first
   end
 
   def peek
+    @store.first
   end
 
   def push(val)
+    @store.push(val)
+    @store = BinaryMinHeap.heapify_up(@store, @store.length-1, @prc)
   end
 
   public
   def self.child_indices(len, parent_index)
+    children = []
+    child1 = (parent_index * 2) + 1
+    child2 = (parent_index * 2) + 2
+    children << child1 if child1 < len
+    children << child2 if child2 < len
+    children
   end
 
   def self.parent_index(child_index)
+    raise "root has no parent" if child_index.zero?
+    (child_index - 1) / 2
   end
 
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
+    prc ? prc : prc = Proc.new { |el1, el2| (el1 <=> el2) }
+    until BinaryMinHeap.child_indices(len, parent_idx).empty?
+      children = BinaryMinHeap.child_indices(len, parent_idx)
+      if children.length == 1 || prc.call(children[0], children[1]) < 0
+        if prc.call(array[children[0]], array[parent_idx]) < 0
+          array[parent_idx], array[children[0]] = array[children[0]], array[parent_idx]
+        end
+      else
+        if prc.call(array[children[1]], array[parent_idx]) < 0
+          array[parent_idx], array[children[1]] = array[children[1]], array[parent_idx]
+        end
+      end
+      parent_idx += 1
+    end
+    array
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    prc ? prc : prc = Proc.new { |el1, el2| (el1 <=> el2) }
+    while child_idx >= 1
+      parent_idx = BinaryMinHeap.parent_index(child_idx)
+      if prc.call(array[child_idx], array[parent_idx]) < 0
+        array[parent_idx], array[child_idx] = array[child_idx], array[parent_idx]
+      end
+      child_idx -= 1
+    end
+    array
   end
 end
