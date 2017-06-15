ffeda50cb9dd7a975e2778b48e42aa2507d4e856
Janice Yura Lee <janiceyuralee@gmail.com>
2017-06-15 12:03:34 -0700

    Change sort algorithm

--- heap_and_heapsort/lib/heap_sort.rb
+++ heap_and_heapsort/lib/heap_sort.rb
@@ -2,13 +2,17 @@ require_relative "heap"
 
 class Array
   def heap_sort!
-    2.upto(length).each do |start|
-      BinaryMinHeap.heapify_up(self, start - 1, start)
+    (0...self.length).each do |start|
+      BinaryMinHeap.heapify_up(self, self.length - 1 - start, self.length - start)
+      self[0], self[self.length - 1 - start] = self[self.length - 1 - start], self[0]
     end
-    length.downto(2).each do |start|
-      self[start - 1], self[0] = self[0], self[start - 1]
-      BinaryMinHeap.heapify_down(self, 0, start - 1)
-    end
-    self.reverse!
+    reverse!
+
+    # BinaryMinHeap.heapify_up(self, self.length - 1, self.length)
+    # self[0], self[-1] = self[-1], self[0]
+    # (0...self.length).each do |start|
+    #   BinaryMinHeap.heapify_down(self, 0, self.length - start)
+    #   self[0], self[self.length - start] = self[self.length - start], self[0]
+    # end
   end
 end
