4f46558f0776e047e0755ff3174f0e76e6aa32cb
Janice Yura Lee <janiceyuralee@gmail.com>
2017-06-18 17:36:26 -0700

    Add binary search tree

--- janice_yura_lee/lib/binary_search_tree.rb
+++ janice_yura_lee/lib/binary_search_tree.rb
@@ -1,34 +1,124 @@
-# There are many ways to implement these methods, feel free to add arguments 
+# There are many ways to implement these methods, feel free to add arguments
 # to methods as you see fit, or to create helper methods.
+require_relative 'bst_node'
 
 class BinarySearchTree
+  attr_reader :root
+
   def initialize
+    @root = nil
   end
 
   def insert(value)
+    new_node = BSTNode.new(value)
+    if @root.nil?
+      @root = new_node
+    else
+      BinarySearchTree.insert_subtree(@root, value)
+    end
   end
 
   def find(value, tree_node = @root)
+    if tree_node.nil?
+      nil
+    else
+      BinarySearchTree.find_subtree(@root, value)
+    end
   end
 
   def delete(value)
+    @root = BinarySearchTree.delete_subtree(@root, value)
   end
 
   # helper method for #delete:
   def maximum(tree_node = @root)
+    BinarySearchTree.maximum(tree_node)
   end
 
   def depth(tree_node = @root)
-  end 
+    if tree_node.nil?
+      0
+    elsif tree_node.left.nil? && tree_node.right.nil?
+      0
+    else
+      [depth(tree_node.left), depth(tree_node.right)].max + 1
+    end
+  end
 
   def is_balanced?(tree_node = @root)
+    return true if tree_node.nil?
+    difference = (depth(tree_node.left) - depth(tree_node.right)).abs
+    left = is_balanced?(tree_node.left)
+    right = is_balanced?(tree_node.right)
+    difference < 1 && left && right
   end
 
   def in_order_traversal(tree_node = @root, arr = [])
+    in_order_traversal(tree_node.left, arr) if tree_node.left
+    arr << tree_node.value
+    in_order_traversal(tree_node.right, arr) if tree_node.right
+    arr
   end
-
 
   private
   # optional helper methods go here:
+  def self.insert_subtree(root, value)
+    new_node = BSTNode.new(value)
+    if root.nil?
+      root = new_node
+    elsif root.value >= value
+      root.left = BinarySearchTree.insert_subtree(root.left, value)
+    else
+      root.right = BinarySearchTree.insert_subtree(root.right, value)
+    end
+    root
+  end
+
+  def self.find_subtree(root, value)
+    if root.nil?
+      nil
+    elsif root.value == value
+      root
+    elsif root.value > value
+      BinarySearchTree.find_subtree(root.left, value)
+    else
+      BinarySearchTree.find_subtree(root.right, value)
+    end
+  end
+
+  def self.delete_subtree(root, value)
+    if root.nil?
+      nil
+    elsif root.value > value
+      root.left = BinarySearchTree.delete_subtree(root.left, value)
+    elsif root.value < value
+      root.right = BinarySearchTree.delete_subtree(root.right, value)
+    else
+      if root.left.nil? && root.right.nil?
+        nil
+      elsif !root.left.nil? && !root.right.nil?
+        max_from_left = BinarySearchTree.maximum(root.left)
+        if max_from_left.left.nil?
+          max_from_left
+        else
+          temp = max_from_left
+          temp = temp.left
+          max_from_left.left = root.left
+          max_from_left.right = root.right
+          max_from_left
+        end
+      else
+        root.left.nil? ? root.right : root.left
+      end
+    end
+  end
+
+  def self.maximum(root)
+    if root.right.nil?
+      root
+    else
+      BinarySearchTree.maximum(root.right)
+    end
+  end
 
 end
--- janice_yura_lee/lib/bst_practical.rb
+++ janice_yura_lee/lib/bst_practical.rb
@@ -1,3 +1,7 @@
+require_relative 'binary_search_tree'
 
 def kth_largest(tree_node, k)
+  bst = BinarySearchTree.new
+  depth = bst.depth(tree_node)
+
 end
